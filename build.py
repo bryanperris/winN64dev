f4f9f46ef9bf34187c9745574af8897d04e25b78
bryanperris <bryanperris@live.com>
2015-07-27 14:54:18 -0400

    External tools called by python now work

--- build.py
+++ build.py
@@ -2,41 +2,35 @@
 import sys
 import os
 import subprocess
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, DEVNULL
 
 print("N64 compiler system 1.0, written in python")
 
 # Setup environment variables
-#os.environ["PATH"] += ";cygwin;"
+os.environ["Path"] = 'cygwin'
 
 # Setup paths
-toolRoot = "mips64-elf"
-toolGNUPrefix = "/mips64-elf-"
+toolRoot = ".\\mips64-elf"
+toolGNUPrefix = "\\mips64-elf-"
 toolSuffix = ".exe"
-toolBin  = toolRoot + "/bin"
-toolLib  = toolRoot + "/lib"
-toolInclude = toolRoot + "/include"
-toolCLib = toolRoot + "/mips64-elf/lib"
+toolBin  = toolRoot + "\\bin"
+toolLib  = toolRoot + "\\lib"
+toolInclude = toolRoot + "\\include"
+toolCLib = toolRoot + "\\mips64-elf\\lib"
 toolGCC = toolBin + toolGNUPrefix + "gcc" + toolSuffix
 toolLD = toolBin + toolGNUPrefix + "ld" + toolSuffix
 toolObj = toolBin + toolGNUPrefix + "objcopy" + toolSuffix
-toolSum = toolBin + "/chksum64" + toolSuffix
-toolMkdfs = toolBin + "/mkdfs" + toolSuffix
-toolSprite = toolBin + "/mksprite" + toolSuffix
-toolN64 = toolBin + "/n64tool" + toolSuffix
-romHeaderFile = toolLib + "/header"
+toolSum = toolBin + "\\chksum64" + toolSuffix
+toolMkdfs = toolBin + "\\mkdfs" + toolSuffix
+toolSprite = toolBin + "\\mksprite" + toolSuffix
+toolN64 = toolBin + "\\n64tool" + toolSuffix
+romHeaderFile = toolLib + "\\header"
 
 def run_tool(command, args):
-        print("Execute: " + command + " " + args)
-        p = Popen(
-            [command, args],
-            stdout = PIPE,
-            stdin = PIPE,
-            stderr = STDOUT,
-            shell = True)
-        out, err = p.communicate()
-        print(out[0].decode())
-
+	print("Execute: " + command + " " + args)
+	p = Popen(command + args, stdout = PIPE, stderr = STDOUT, shell = True)
+	out = p.communicate()
+	if len(out[0]) > 0:
+		print(out[0].decode())
 
 # ---- Where the build system begins -----
-run_tool(toolN64, '')
