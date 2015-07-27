9b2abe1b33e093c7d4cd8693ae4e876accfdd32c
bryanperris <bryanperris@live.com>
2015-07-27 16:02:06 -0400

    implement function to make elf into n64 rom file.  A fix in run_tool function.

--- build.py
+++ build.py
@@ -27,10 +27,21 @@ toolN64 = toolBin + "\\n64tool" + toolSuffix
 romHeaderFile = toolLib + "\\header"
 
 def run_tool(command, args):
-	print("Execute: " + command + " " + args)
-	p = Popen(command + args, stdout = PIPE, stderr = STDOUT, shell = True)
+	print("Execute: " + command + ' ' + args)
+	p = Popen(command + ' ' + args, stdout = PIPE, stderr = STDOUT, shell = True)
 	out = p.communicate()
 	if len(out[0]) > 0:
 		print(out[0].decode())
+		
+def makeRom(target):
+	run_tool(toolObj, target + '.elf' + ' ' + target + '.bin -O binary')
+	
+	try : 
+		os.remove(target)
+	except :
+		pass
+		
+	run_tool(toolN64, '-b -l 2M -t ' + os.path.splitext(target + '.elf')[0] + ' h' + ' -o ' + target + '.v64 ' + target + '.bin ')
+	run_tool(toolSum, target + '.v64')
 
 # ---- Where the build system begins -----
